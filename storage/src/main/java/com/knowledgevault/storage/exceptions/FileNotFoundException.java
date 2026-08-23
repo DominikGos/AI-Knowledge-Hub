d23521b853901973186b3fb70cc80c3f9b943afe
DominikGos <dominikgos3@gmail.com>
2026-08-23 19:36:51 +0200

    Added exception statuses

--- storage/src/main/java/com/knowledgevault/storage/exceptions/FileNotFoundException.java
+++ storage/src/main/java/com/knowledgevault/storage/exceptions/FileNotFoundException.java
@@ -1,5 +1,9 @@
 package com.knowledgevault.storage.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class FileNotFoundException extends RuntimeException {
     public FileNotFoundException(String message) {
         super(message);
