3eaa3b5c4ebeeec5e1f3aae4fa01a1368a7dff1b
DominikGos <dominikgos3@gmail.com>
2026-08-31 16:51:12 +0200

    Moved the response statuses to the exception handler

--- storage/src/main/java/com/knowledgevault/storage/exceptions/FileValidationException.java
+++ storage/src/main/java/com/knowledgevault/storage/exceptions/FileValidationException.java
@@ -1,9 +1,5 @@
 package com.knowledgevault.storage.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class FileValidationException extends RuntimeException {
     public FileValidationException(String message) {
         super(message);
--- storage/src/main/java/com/knowledgevault/storage/exceptions/StorageException.java
+++ storage/src/main/java/com/knowledgevault/storage/exceptions/StorageException.java
@@ -1,9 +1,5 @@
 package com.knowledgevault.storage.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class StorageException extends RuntimeException {
 
     public StorageException(String message) {
