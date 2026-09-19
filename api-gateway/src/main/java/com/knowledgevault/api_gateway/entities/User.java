feeb6d5adbd0c3196e5477bd8b7aeb623669381b
DominikGos <dominikgos3@gmail.com>
2026-09-19 12:38:59 +0200

    Added user repo and implemented user details

--- api-gateway/src/main/java/com/knowledgevault/api_gateway/entities/User.java
+++ api-gateway/src/main/java/com/knowledgevault/api_gateway/entities/User.java
@@ -2,8 +2,12 @@ package com.knowledgevault.api_gateway.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -13,7 +17,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -33,4 +37,34 @@ public class User {
 
     @Column(name = "updated_at")
     private Instant updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ api-gateway/src/main/java/com/knowledgevault/api_gateway/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.knowledgevault.api_gateway.repositories;
+
+import com.knowledgevault.api_gateway.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
